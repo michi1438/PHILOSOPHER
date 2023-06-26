1ae02e7b806a6cd364c0b9c30e9ef90d1294acde
michael guerg <michaelguerga@gmail.com>
2023-06-26 16:42:44 +0200

    add philo_bonus

---
+++ philo_bonus/head_philo_bonus.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   head_philo_bonus.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mguerga <mguerga@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/26 13:12:17 by mguerga           #+#    #+#             */
+/*   Updated: 2023/06/26 13:16:10 by mguerga          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEAD_PHILO_BONUS_H
+# define HEAD_PHILO_BONUS_H
+
+# include <stdio.h>
+# include <string.h>
+# include <pthread.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+
+//PHILO_MAIN.C
+
+#endif
