6bb57aed26b51e32cc2862b3faffe3efccecd305
michael guerg <michaelguerga@gmail.com>
2023-07-21 16:06:54 +0200

    to push to mac

--- philo_bonus/src/bonus_philo_invok.c
+++ philo_bonus/src/bonus_philo_invok.c
@@ -6,7 +6,7 @@
 /*   By: mguerga <mguerga@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 16:05:20 by mguerga           #+#    #+#             */
-/*   Updated: 2023/07/20 10:14:05 by mguerga          ###   ########.fr       */
+/*   Updated: 2023/07/20 15:32:50 by mguerga          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ int	child_play(t_philos *philos)
 		print_log(philos->process[0], FORK);
 		sem_post(philos->semaphore_wwait2);
 		pthread_detach(philos->thread);
-		check_for_death(philos);
+//		check_for_death(philos);
 		is_eating(philos, comp);
 		sleep_timer(philos, comp);
 	}
@@ -81,18 +81,20 @@ void	*death_wwait(t_philos *philos)
 void	*check_for_death(t_philos *philos)
 {
 	unsigned long	act_time;
+	unsigned long	tdeath;
 	t_comp			*comp;
 
 	comp = &philos->compend;
 	act_time = actual_time();
 	sem_wait(philos->semaphore_wwait);
-	if (act_time - comp->tv_has_eaten >= (unsigned long)comp->t_death)
+	tdeath = comp->t_death;
+	if (act_time - comp->tv_has_eaten >= tdeath \
+			&& !(act_time - comp->tv_has_eaten > tdeath * 2))
 	{
 		print_log(philos->process[0], DIE);
 		exit (0);
 	}
 	sem_post(philos->semaphore_wwait);
-//	printf("passed %d\n", philos->process[0] + 1);
 	return (NULL);
 }
 
